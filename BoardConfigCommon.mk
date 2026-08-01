b935d05be997908f8a814d2d041ec602183f4631
Bush-cat <bushccat@gmail.com>
2026-08-01 16:02:22 +0200

    DNM sm7225-common: Import proprietary-files
    Change-Id: I62275ee06b10cb5a4e37a9fe0ab213c3f6fb8beb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,7 +170,7 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_samsung_sm7225
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Security patch
-VENDOR_SECURITY_PATCH := 2025-10-01
+VENDOR_SECURITY_PATCH := 2026-02-05
 
 # SePolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
